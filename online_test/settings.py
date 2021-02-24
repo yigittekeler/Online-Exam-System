c7220f99d82eb80842d5ff23c3bd478aeb5de2c2
yigittekeler <76699902+yigittekeler@users.noreply.github.com>
2021-02-24 22:08:32 +0300

    Update settings.py

--- online_test/settings.py
+++ online_test/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 
@@ -123,5 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(os.path.normpath(BASE_DIR), "static"))
-STATIC_ROOT = os.path.join(os.path.normpath(BASE_DIR), "gorunum")
+
